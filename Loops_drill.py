4002a01853e2542e8c8f3d7c6fcb82db6c5cbab2
leblangjacobita1 <57274817+leblangjacobita1@users.noreply.github.com>
2020-02-15 17:14:39 -0600

    Initial commit

---
+++ Loops_drill.py
@@ -0,0 +1,84 @@
+'''
+Created on Feb 15, 2020
+
+@author: ITAUser
+'''
+
+'''
+For this assignment you should read the task, then below the task do what it asks you to do
+based on what the task tells you do first.
+EXAMPLE TASK:
+'''
+#EX) Declare a variable set to 3. Make a while loop that prints the variable 
+#    you just created and decrements the variable by one each time through
+#    the loop. Meanwhile, make the loop run until the variable you created 
+#    equals 0.
+i = 3;
+while i > 0:
+    print(i)
+    i -= 1
+
+'''
+END OF EXAMPLE
+'''
+
+'''
+START HERE
+'''
+
+'''While Loops'''
+#1) Declare a variable set to 4. Make a while loop that prints the variable 
+#    you just created and decrements the variable by one each time through
+#    the loop. Meanwhile, make the loop run until the variable you created 
+#    equals 1.
+x=4
+
+for x in range(4, 1):
+    print(x)
+
+
+
+
+#2) Declare a variable set to 14. Make a while loop that prints the variable 
+#    you just created and increments the variable by one each time through
+#    the loop. Meanwhile, make the loop run until the variable you created 
+#    equals 20.
+
+y = 14
+while y!=(21):
+    print(y)
+    y += 1
+    if (y==21):
+        break
+
+
+#3) Declare a variable set to 55. Make a while loop that prints the variable 
+#   you just created. Then make an if statement that makes the loop break when
+#   the variable is equal to 50. 
+
+
+z=35
+while z!=(51):
+    print(z)
+    z += 1
+    if (z==51): 
+        break
+'''For Loops'''
+#4) Create a list named sports. Put three sports into the list. Create
+#   a for loop that prints each sport in the list
+
+sports = ["Basketball","Soccer", "Baseball"]
+for i in list:
+        print(i)
+
+#5) Create a for loop that loops through each letter in a string of one of your
+#   favorite songs. Each iteration should print should a letter of the word. 
+
+
+
+
+#6) Create a list named movies. Put five of your favorite movies into the list.
+#   However, make sure one of the movies is Avatar. 
+#   Create a for loop that iterates over the list. In the loop print the movie
+#   being looped over, but create an if statement that breaks out of the 
+#   loop if it is Avatar.
